af3f290066ca8c47c97d6db6eb4affbdc85463ef
Arctessa <arctessa@ravenswurk.frl>
2026-05-24 14:28:56 -0700

    GUI: Update layout
    Based on recommendations from Iantm from 68KMLA this commit tweaks
    our GUI's layout to have categorised sections. Additionally makes it
    so our pcap button enables and disables the file browser versus
    hiding and showing it.

--- tailtalk-gui/build.rs
+++ tailtalk-gui/build.rs
@@ -1,3 +1,7 @@
 fn main() {
-    slint_build::compile("ui/app.slint").unwrap();
+    slint_build::compile_with_config(
+        "ui/app.slint",
+        slint_build::CompilerConfiguration::new().with_style("native".into()),
+    )
+    .unwrap();
 }
